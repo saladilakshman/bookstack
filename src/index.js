0e3bb2a2fcd94e041a588cb670cf1d399544fbf5
saladilakshman <103055304+saladilakshman@users.noreply.github.com>
2023-04-11 15:50:05 +0530

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 const Apex=React.lazy(()=>import('./App'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.Suspense fallback={<div style={{display:'flex',
   justifyContent:'center',
   alignItems:'center',marginBlockStart:'15rem'}}>
-    <div className="spinner-border text-primary"></div>
+    <div style={{fontFamily:'helvetica',color:'steelblue',fontSize:'1.5rem'}}>Loading...</div>
   </div>}>
     <Apex/>
   </React.Suspense>
